d1afdfbccc3e2167d7329d1d3262ad7cee0bdbe7
Thomas Backlund <thomas@superblocks.com>
2019-10-01 15:09:41 +0200

    Update truffle-config.js to use Superprovider

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const SuperProvider = require('./superprovider.js');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +9,16 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
-    }
+    },
+    rinkeby: {
+      provider: function() { return new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY) },
+      network_id: 4
+    },
+    rinkeby_metamask: {
+        provider: () => {
+            return new SuperProvider(process.env.SUPERBLOCKS_SESSIONID, process.env.SUPERBLOCKS_ADDRESS, {proxyUrl: process.env.WEB3_ENDPOINT})
+        },
+        network_id: '4'
+    },
   }
 };
